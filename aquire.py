a92ecba3af0906288dcfcfe231b591ceaf18c8a8
Glady Barrios <Barrios.glady01@gmail.com>
2022-08-31 16:58:49 -0500

    adding

---
+++ aquire.py
@@ -0,0 +1,68 @@
+#Import dependencies
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import os
+import json
+
+
+def get_blog_articles_data(refresh=False):
+    
+    if not os.path.isfile('blog_articles.csv') or refresh:
+        
+        url = 'https://codeup.com/blog/'
+        headers = {'User-Agent': 'Codeup Data Science'}
+        response = get(url, headers=headers)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        links = [link['href'] for link in soup.select('h2 a[href]')]
+
+        articles = []
+
+        for url in links:
+
+            url_response = get(url, headers=headers)
+            soup = BeautifulSoup(url_response.text, 'html.parser')
+
+            title = soup.find('h1', class_='entry-title').text
+            content = soup.find('div', class_='entry-content').text.strip()
+
+            article_dict = {
+                'title': title,
+                'content': content
+            }
+
+            articles.append(article_dict)
+        
+        blog_article_df = pd.DataFrame(articles)
+        
+        blog_article_df.to_csv('blog_articles.csv', index=False)
+        
+    return pd.read_csv('blog_articles.csv')
+
+ #Define a function that will scrape information about an array of topics
+def get_news_articles():
+    
+    file = 'news_articles.json'
+    
+    if os.path.exists(file):
+        
+        with open(file) as f:
+            
+            return json.load(f)
+    
+    topic_list = ['business', 'sports', 'technology', 'entertainment']
+    
+    final_list = []
+    
+    for topic in topic_list:
+        
+        final_list.extend(scrape_one_page(topic))
+        
+    with open(file, 'w') as f:
+        
+        json.dump(final_list, f)
+        
+    return final_list      
